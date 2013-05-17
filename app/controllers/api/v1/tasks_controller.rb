87b5983e12e8c7dbb04201a870f6cbe2f8f80b0d
Akshat Divekar <akshatd@evidentpoint.com>
2013-05-17 15:09:03 -0700

    Working Application

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class TasksController < BaseController
+      doorkeeper_for :all
+      respond_to :json
+
+      def index
+        respond_with current_user.tasks
+      end
+
+      def create
+        respond_with current_user.tasks.create(params[:task])
+      end
+
+    end
+  end
+end
